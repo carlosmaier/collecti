4e0b78430312b496ca37f391d23659fc7e89e02a
carlosmaier <carlosmaier@chicagobooth.edu>
2023-03-07 21:38:50 +0000

    progress

--- app/models/exchange.rb
+++ app/models/exchange.rb
@@ -16,7 +16,7 @@ class Exchange < ApplicationRecord
 
   def Exchange.check_request(album_id,sender_id,card_send,receiver_id,card_receive)
 
-    request = Exchange.where({album_id: album_id}).where({id_sender: sender_id}).where({card_number_to_send: card_send}).where({id_receiver: receiver_id}).where({card_number_to_receive: card_receive})
+    request = Exchange.where({album_id: album_id}).where({id_sender: sender_id}).where({card_number_to_send: card_send}).where({id_receiver: receiver_id}).where({card_number_to_receive: card_receive}).where.not({status: "cancelled"})
 
     if request.count >= 1 
       check = true
